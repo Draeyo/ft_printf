e73ef16d3d0960768fc7f83c931610f3d015499a
Victor LISTRAT <vlistrat@e2r10p6.42.fr>
2016-01-07 20:47:04 +0100

    ll

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vlistrat <vlistrat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:22:42 by vlistrat          #+#    #+#             */
-/*   Updated: 2016/01/06 17:27:23 by vlistrat         ###   ########.fr       */
+/*   Updated: 2016/01/07 20:46:59 by vlistrat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int	ft_printf(const char *format, ...)
 {
-	// return (str_len);
+	int		cnt;
+
+	cnt = 0;
+	while (*format)
+	{
+		if (*format == '%')
+			cnt+= ft_conv(&format);
+		ft_putchar(*format);
+		cnt++;
+	}
+	return (cnt);
 }
